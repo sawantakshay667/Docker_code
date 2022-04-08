a46d302496ca25ef599278605404d7ff432883ca
AkshayVSCode <sawantakshay667@gmail.com>
2022-04-08 17:26:41 +0530

    docker-windows cmd added for Nginx

--- Nginx-docker in aws.sh
+++ Nginx-docker in aws.sh
@@ -19,5 +19,8 @@ sudo docker pull nginx
 # sudo mkdir ~/docker-share
 # sudo mkdir ~/docker-share/html
 
-# Docker nginx use port 8080 to public ip
+# Docker nginx use port 8080 to public ip && trafer file from ~/docker-share/html to /usr/share/nginx/html && tag name nginx-container (For Linux Disto)
 # sudo docker run -d -p 8080:80 -v ~/docker-share/html:/usr/share/nginx/html --hostname nginx-container nginx
+
+# Docker nginx use port 8080 to public ip && trafer file from E:/website/PetLover to /usr/share/nginx/html && tag name web-server (For Windows Docker installation)
+# docker run -d -p8080:80 -v E:/website/PetLover:/usr/share/nginx/html --name web-server nginx
